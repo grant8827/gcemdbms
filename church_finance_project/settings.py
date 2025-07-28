1d2fba77b29cb2156a06bc80f42445de6887d399
Gregory Grant <grant8827@yahoo.com>
2025-07-28 13:20:50 -0400

    comment out a database url

--- church_finance_project/settings.py
+++ church_finance_project/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = "church_finance_project.wsgi.application"
 #  }
 # }
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-    )
-}
+#DATABASES = {
+#    "default": dj_database_url.config(
+#        default=os.environ.get("DATABASE_URL"),
+#        conn_max_age=600,
+#    )
+# }
 
 DATABASES = {
      'default': {
